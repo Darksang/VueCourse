e7513a5762697334b7ddbd47970bd3d369199249
Sergio Dorado <darksang.exodar@gmail.com>
2023-04-18 09:52:24 +0200

    Push first 3 projects

---
+++ quizApp/src/components/Result.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import { defineProps } from "vue"
+    import { RouterLink } from "vue-router"
+
+    const { quizQuestionLength, correctAnswers } = defineProps(["quizQuestionLength", "correctAnswers"])
+</script>
+
+<template>
+    <div class="results">
+        <p>Your Results...</p>
+        <h1>
+            {{ correctAnswers }}/{{ quizQuestionLength }}
+        </h1>
+        <RouterLink to="/">Go Back</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+    .results {
+        text-align: center;
+        padding: 100px 0;
+    }
+
+    p {
+        font-size: 25px;
+    }
+
+    h1 {
+        font-size: 80px;
+    }
+</style>
